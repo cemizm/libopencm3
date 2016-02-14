93fbf1f907fc4fa1a16f1dc81a87b1db58517878
Cem Basoglu <cem.basoglu@web.de>
2016-02-14 17:05:48 +0100

    STM32F3: CAN port register

--- lib/stm32/can.c
+++ lib/stm32/can.c
@@ -41,6 +41,8 @@ LGPL License Terms @ref lgpl_license
 #	include <libopencm3/stm32/f1/rcc.h>
 #elif defined(STM32F2)
 #	include <libopencm3/stm32/f2/rcc.h>
+#elif defined(STM32F3)
+#	include <libopencm3/stm32/f3/rcc.h>
 #elif defined(STM32F4)
 #	include <libopencm3/stm32/f4/rcc.h>
 #else
@@ -71,6 +73,12 @@ can_reg_base.
  */
 void can_reset(uint32_t canport)
 {
+#if defined(STM32F3)
+	if (canport == CAN1) {
+			rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CANRST);
+			rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CANRST);
+	}
+#else
 	if (canport == CAN1) {
 		rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN1RST);
 		rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN1RST);
@@ -78,6 +86,7 @@ void can_reset(uint32_t canport)
 		rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN2RST);
 		rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1RSTR_CAN2RST);
 	}
+#endif
 }
 
 /*---------------------------------------------------------------------------*/
